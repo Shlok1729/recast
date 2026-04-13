dd6ba760454ca4fc6412d77aab5fe3df9d981386
kanakkholwal <kanakkholwal@gmail.com>
2026-04-13 23:01:47 +0530

    chore: update package manager to pnpm@10.23.0 and add pnpm workspace configuration

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,18 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html' // may differ from host to host
+			// See below for an explanation of these options
+			config: undefined,
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			},
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
 		}),
 		alias: {
 			$components: 'src/components',
